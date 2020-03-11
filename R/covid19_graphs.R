23b89471c2db0de5adc86b4ab2e2c1fc5512130c
Marcelo Ponce <mponce@scinet.utoronto.ca>
2020-03-11 16:45:58 -0400

    generalizign plotting fn

--- R/covid19_graphs.R
+++ R/covid19_graphs.R
@@ -1,18 +1,34 @@
-totals <- function() {
+totals <- function(data=NULL, geo.loc=NULL) {
 
-	total.cases <- covid19()
-	col1 <-5; colN <- ncol(total.cases)-1
+	if (is.null(data)) {
+		total.cases <- covid19()
+	} else {
+		total.cases <- data
+	}
+
+	if (!is.null(geo.loc)) {
+		# check geographical location
+		geo.loc <- checkGeoLoc(geo.loc)
+
+		total.cases <- select.per.region(total.cases,geo.loc)
+	}
+
+	col1 <-5; colN <- ncol(total.cases)
+	if (tolower("status") %in% colnames(totalcases)) colN <- colN-1
 
 	totals <- apply(total.cases[,col1:colN], MARGIN=2,sum)
 
+	###
 	categories <- unique(total.cases$status)
-	totals.per.cat <- c()
+	totals.per.cat <- data.frame()
 	for (categ in seq_along(categories)) {
-		totals.per.cat[categ] <- apply(total.cases[total.cases$status==categ,col1:colN], MARGIN=2,sum)
+		totals.per.cat <- c(totals.per.cat, c(categories[categ],apply(total.cases[total.cases$status==categ,col1:colN], MARGIN=2,sum) )  )
 	}
+	##
 	confirmed <- apply(total.cases[total.cases$status=="confirmed",col1:colN], MARGIN=2,sum)
 	recovered <- apply(total.cases[total.cases$status=="recovered",col1:colN], MARGIN=2,sum)
 	deaths <- apply(total.cases[total.cases$status=="death",col1:colN], MARGIN=2,sum)
+	###
 
 	x.dates <- as.Date(names(total.cases)[col1:colN])
 	ymax <- max(totals)
@@ -34,6 +50,8 @@ totals <- function() {
 	fig <- fig %>% add_trace(y = ~confirmed, name="confirmed", type='scatter', mode='lines+markers')
 	fig <- fig %>% add_trace(y = ~recovered, name="recovered", type='scatter', mode='lines+markers')
 	fig <- fig %>% add_trace(y = ~deaths, name="deaths", type='scatter', mode='lines+markers')
+
+	return(totals.per.cat)
 }
 
 
