b3b86e35a51c9b688b51067c49fde8b0dfc1c6ab
Marcelo Ponce <mponce@scinet.utoronto.ca>
2020-03-11 22:25:39 -0400

    added fn to cehck and load libraries

--- R/covid19_auxs.R
+++ R/covid19_auxs.R
@@ -76,3 +76,18 @@ checkGeoLoc <- function(data, geo.loc=NULL) {
 ############################################################################
 
 
+
+##########################################################################
+
+
+# load and check needed packages/libraries...
+loadLibrary <- function(lib) {
+#' function to check and load an specific set of libraries
+#' @param  lib  is a list of packages to be loaded
+#' @keywords internal
+
+        if (require(lib,character.only = TRUE) == FALSE) stop(lib, " is needed for this package to work but is not installed in your system!")
+}
+
+
+#########################################################################
