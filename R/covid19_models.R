06628b24d99ff9e582abac46ac042e2b29a85625
Marcelo Ponce <mponce@scinet.utoronto.ca>
2020-03-16 18:00:07 -0400

    new module to host models

---
+++ R/covid19_models.R
@@ -0,0 +1,57 @@
+# Modelling fns of the covid19 package
+#
+# M.Ponce
+
+
+#######################################################################
+
+genModel <- function(y, deg=1) {
+#' function to generate models using Linear Regression "LM"
+#'
+#' @keywords internal
+#'
+#' @importFrom  stats  lm
+#'
+
+	y.var <- unlist(y)
+	x.var <- 1:length(y.var)
+	#print(x.var)
+	#print(y.var)
+
+	model <- lm(y.var ~ x.var)
+
+	cat("Linear Regression (lm):", '\n')
+	print(summary(model))
+
+	return(model)	
+}
+
+evalModel <- function(model) {
+
+}
+
+
+#######################################################################
+
+gen.glm.model <- function (y, family=Gamma(link="log")) {
+#' function to generate models using GLM
+#'
+#' @keywords internal
+#'
+#' @importFrom  stats  glm Gamma poisson
+#'
+	y.var <- unlist(y)
+	x.var <- 1:length(y.var)
+	#print(x.var)
+	#print(y.var)
+
+	model <- glm(y.var ~ x.var, family=family)
+
+	cat(paste0("GLM using Family=",paste(family,collpase=' ')," :"),'\n')
+	print(summary(model))
+
+	return(model)
+}
+
+#######################################################################
+
