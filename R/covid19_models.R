298192233ab95bacab569bb013f20b0a682d4282
Marcelo Ponce <mponce@scinet.utoronto.ca>
2020-03-22 19:45:59 -0400

    more on SIR models

--- R/covid19_models.R
+++ R/covid19_models.R
@@ -78,14 +78,19 @@ simple.SIR.ODE <- function(time, state, parameters) {
 
 
 simple.SIR.solver <- function(data=NULL, geo.loc="Hubei",
-				t0=30,t1=45, tfinal=90,
+				t0=NULL,t1=NULL, deltaT=NULL,
+				tfinal=90,
 				fatality.rate = 0.02,
 				tot.population=1400000000) {
 #' function to generate a simple SIR model
 #'
 #' @param  data  dataset to consider
 #' @param  geo.loc  country/region to analyze
+#' @param  t0  initial period of time for data consideration
+#' @param  t1  final period of time for data consideration
+#' @param  deltaT interval period of time from t0, ie. number of days to consider since t0
 #' @param  tfinal  total number of days
+#' @param  fatality.rate  rate of causality, deafault value of 2%
 #' @param  tot.population  total population of the country/region
 #'
 #' @importFrom  stats  optim setNames
@@ -94,35 +99,86 @@ simple.SIR.solver <- function(data=NULL, geo.loc="Hubei",
 #'
 #' @export
 #'
+#' @examples
+#' data <- covid19.data("confirmed")
+#' simple.SIR.solver(data,"Hubei")
+#' simple.SIR.solver(data,"Germany",tot.population=83149300)
+#' simple.SIR.solver(data,"Uruguay", tot.population=3500000)
+#'
 
-	header <- function(x="-",title="",total.len=80,eol='\n') {
-		len.title <- nchar(title)
-		if (len.title !=0) {
-			reps <- (total.len-len.title)/2
-			header(x,total.len=reps,eol=' ')
-			cat(title)
-			header(x,total.len=reps,eol='\n')
-		} else {
-			cat(paste(paste(rep(as.character(x),total.len),collapse=""),eol))
-		}
-	}
-
+	# DISCLAIMER // EXPERIMENTAL FEATURES
 	header("#")
 	message("This is an experimental feature, being currently under active development!")
 	message("Please check the development version of the package for the latest updates on it")
 	header("#")
 
-	header('-'," Parameters used to create model ")
+
+	# get actual data from indicated region...
+	pot.Infected <- preProcessingData(data,geo.loc)
+	# eliminate entries without cases, ie equal 0
+	#pot.Infected <- pot.Infected[cumsum(pot.Infected)!=0]
+	print(pot.Infected)
+
+
+        # default values
+        t0.default=30
+        deltaT.default=25
+        t1.default=t0.default+deltaT.default
+        ###########
+	
+	if (is.null(t0)) {
+		# attempt to auto-detect significant growth....
+		# will use a moving average of 2 days...
+		#x <- movingFn(pot.Infected,period=2)
+		x <- pot.Infected
+		print(x)
+		l1 <- length(x)
+		l2 <- l1-1
+		threashold <- 10
+		t0 <- min(which(cumsum( (x[2:l1]-x[1:l2]) > threashold) != 0),na.rm=TRUE)
+		print(t0)
+		if (!is.null(t1)) {
+			if (t1>t0) {
+				Infected <- pot.Infected[t0:min(t1,l1)]
+			} else {
+				Infected <- pot.Infected[t0:min(t0+deltaT.default,l1)]
+			}
+		} else if (!is.null(deltaT)) {
+				Infected <- pot.Infected[t0:min(t0+deltaT,l1)]
+			} else {
+				Infected <- pot.Infected[t0:min(t0+deltaT.default,l1)]
+			}
+		print(Infected)
+	} else if (!is.null(t1)) {
+			# user indicated t0 & t1
+			Infected <- pot.Infected[t0:t1]
+		} else if (!is.null(deltaT)) {
+				# user indicated t0 & deltaT
+				Infected <- pot.Infected[t0:(t0+deltaT)]
+			} else {
+				Infected <- pot.Infected
+			}
+
+
+#	if (is.null(t0) & is.null(t1) & is.null(deltaT)) {
+#		t0 <- t0.default
+#		t1 <- t1.default
+#	}
+
+	print(Infected)
+
+
+	###########
+	header('-',"Parameters used to create model ")
 	cat('\t',"Region: ",geo.loc,'\n')
 	cat('\t',"Time interval to consider: t0=",t0," - t1=",t1," ; tfinal=",tfinal,'\n')
+	cat('\t\t',"t0: ",names(data)[t0]," -- t1: ",names(data)[t1],'\n')
+	cat('\t',"Number of days considered for initial guess: ",length(Infected),'\n')
 	cat('\t',"Fatality rate: ",fatality.rate,'\n')
 	cat('\t',"Population of the region: ",tot.population,'\n')
 	header('-')
+	###########
 
-	# get actual data from indicated region...
-	Infected <- preProcessingData(data,geo.loc)
-	Infected <- Infected[cumsum(Infected)!=0]
-	Infected <- Infected[t0:t1]
 	print(Infected)
 
 	# total population of the region
@@ -174,7 +230,7 @@ simple.SIR.solver <- function(data=NULL, geo.loc="Hubei",
 	time <- 1:tfinal # time in days
 	fit <- data.frame(ode(y = init.cond, times = time, func = simple.SIR.ODE, parms = Opt_par))
 
-	col <- 1:3 # colour
+	col <- c("blue","red","green") 
  
 	matplot(fit$time, fit[ , 2:4], type = "l", xlab = "Day", ylab = "Number of subjects", lwd = 2, lty = 1, col = col)
 	matplot(fit$time, fit[ , 2:4], type = "l", xlab = "Day", ylab = "Number of subjects", lwd = 2, lty = 1, col = col, log = "y")
@@ -190,12 +246,19 @@ simple.SIR.solver <- function(data=NULL, geo.loc="Hubei",
 	print(R0)
  
 	# height of pandemic
-	cat("Max of infecteded:")
-	print(fit[fit$I == max(fit$I), "I", drop = FALSE]) 
+	max.I <- max(fit$I)
+	max.I.time <- fit$time[fit$I==max.I]
+	cat("Max of infecteded:", max.I, " (",(max.I/tot.population)*100,"%)",'\n')
+	#print(fit[fit$I == max(fit$I), "I", drop = FALSE]) 
 
 	# Max number of casualties
-	cat("Max nbr of casualties, with ", fatality.rate*100,"% fatality rate:")
-	print(max(fit$I) * fatality.rate)
+	cat("Max nbr of casualties, with ", paste0(fatality.rate*100,"% fatality rate:"),
+		max.I*fatality.rate,'\n')
+	cat("Max reached at day :", max.I.time,
+		'==> ')
+	print(as.Date(names(data)[t0])+max.I.time)
+
+	header("=")
 }
 
 #######################################################################
