018ac8286d9fad84364f224969a4fdaa6f6211dd
Marcelo Ponce <mponce@scinet.utoronto.ca>
2020-03-28 10:43:45 -0400

    adding considerations for long titleS

--- R/aux_utils.R
+++ R/aux_utils.R
@@ -20,6 +20,7 @@ header <- function(x="-",title="",total.len=80,eol='\n') {
 	len.title <- nchar(title)
 	if (len.title !=0) {
 		reps <- (total.len-len.title)/2
+		if (reps < 0) reps <- 1
 		header(x,total.len=reps,eol=' ')
 		cat(title)
 		header(x,total.len=reps,eol='\n')
