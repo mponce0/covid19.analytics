9cbbc29c5029787b17609d890368eb616e4170c5
Marcelo Ponce <mponce@scinet.utoronto.ca>
2020-04-24 18:05:19 -0400

    updated datasets and new data for US

--- inst/extdata/update_DS.sh
+++ inst/extdata/update_DS.sh
@@ -5,7 +5,8 @@
 JHUurl="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/"
 TSdomain="csse_covid_19_time_series"
 AGGdomain="csse_covid_19_daily_reports"
-TSfiles="time_series_covid19_confirmed_global.csv time_series_covid19_deaths_global.csv time_series_covid19_recovered_global.csv"
+TSfiles="time_series_covid19_confirmed_global.csv time_series_covid19_deaths_global.csv time_series_covid19_recovered_global.csv 
+	time_series_covid19_confirmed_US.csv time_series_covid19_deaths_US.csv"
 
 ### Time Series data
 for file in `echo ${TSfiles}`; do
